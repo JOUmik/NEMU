2fc80f8ce60243e4471700cd6e2d280ea83aaacd
3019244195-Liu Yan <1967927796@qq.com>
2020-11-02 22:39:58 +0800

    ..

--- nemu/src/cpu/exec/con-trans/jbe-template.h
+++ nemu/src/cpu/exec/con-trans/jbe-template.h
@@ -4,7 +4,7 @@
 
 
 static void do_execute() {
-	DATA_TYPE_S displacdawement = op_src->val;
+	DATA_TYPE_S displacement = op_src->val;
 	print_asm("jbe %x",cpu.eip + 1 + DATA_BYTE + displacement);
 	if (cpu.CF == 1 || cpu.ZF == 1)cpu.eip +=displacement;
 }
