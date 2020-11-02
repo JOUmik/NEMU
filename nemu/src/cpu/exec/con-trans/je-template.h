bbd7c86be0745154802cc31727688baffac3c830
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-02 23:03:43 +0800

    >  run 3019244195 liuyan Linux liuyan-VirtualBox 5.3.0-53-generic #47~18.04.1-Ubuntu SMP Thu May 7 13:10:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  23:03:43 up 1 day,  2:59,  1 user,  load average: 0.14, 0.08, 0.04 ac7378ad97f39667ffa7d7473e1554f3eaebc270

--- nemu/src/cpu/exec/con-trans/je-template.h
+++ nemu/src/cpu/exec/con-trans/je-template.h
@@ -3,7 +3,7 @@
 #define instr je
 
 
-static void do_execute()afla {
+static void do_execute() {
 	DATA_TYPE_S displacement = op_src->val;
 	print_asm("je %x",cpu.eip + 1 + DATA_BYTE + displacement);
 	if (cpu.ZF == 1)cpu.eip +=displacement;
