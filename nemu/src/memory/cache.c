38e2fac2922bde74c677e981ad1e7402c64aa325
3019244195-Liu Yan <1967927796@qq.com>
2021-01-16 22:54:09 +0800

    cache.c write_cache1

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -62,4 +62,28 @@ int read_cache1(hwaddr_t address){
     return i;
 }
 
+void write_cache1(hwaddr_t addr, size_t len, uint32_t data){
+    uint32_t group_idx = (addr >> Cache_L1_Block_Bit) & (Cache_L1_Group_Size - 1);
+    uint32_t tag = (addr >> (Cache_L1_Group_Bit + Cache_L1_Block_Bit));
+    uint32_t offset = addr & (Cache_L1_Block_Size - 1);
 
+    int i,group = group_idx * Cache_L1_Way_Size;
+    for (i = group + 0;i < group + Cache_L1_Way_Size;i ++){
+        if (cache1[i].valid == 1 && cache1[i].tag == tag){// WRITE HIT
+            /*write through*/
+            if (offset + len > Cache_L1_Block_Size){
+                dram_write(addr,Cache_L1_Block_Size - offset,data);
+                memcpy(cache1[i].data + offset, &data, Cache_L1_Block_Size - offset);
+            
+                write_cache1(addr + Cache_L1_Block_Size - offset,len - (Cache_L1_Block_Size - offset),data >> (Cache_L1_Block_Size - offset));
+            }
+	    else {
+                dram_write(addr,len,data);
+                memcpy(cache1[i].data + offset, &data, len);
+	    }
+	    return;
+	}
+    }
+
+    dram_write(addr,len,data);
+}
