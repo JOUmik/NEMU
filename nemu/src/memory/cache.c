a66810ed7b9b233dd0bdaba3df05c4faae99dc8a
3019244195-Liu Yan <1967927796@qq.com>
2021-01-16 22:49:19 +0800

    cache.h and cache.c read_cache1

---
+++ nemu/src/memory/cache.c
@@ -0,0 +1,65 @@
+#include "common.h"
+#include "memory/cache.h"
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "burst.h"
+
+void public_ddr3_read(hwaddr_t addr, void* data);
+void public_ddr3_write(hwaddr_t addr, void* data, uint8_t *mask);
+
+void dram_write(hwaddr_t addr, size_t len, uint32_t data);
+
+void init_cache(){
+    //initialize cache L1
+    int i;
+    for (i=0; i < Cache_L1_Size / Cache_L1_Block_Size; i++){
+	cache1[i].valid = 0;
+    }
+
+    test_time = 0;
+}
+
+int read_cache1(hwaddr_t address){
+
+    uint32_t group_id = (address >> Cache_L1_Block_Bit) & (Cache_L1_Group_Size - 1);
+    uint32_t tag_id = (address >> (Cache_L1_Block_Bit + Cache_L1_Group_Bit));
+    uint32_t block_start = (address >> Cache_L1_Block_Bit) << Cache_L1_Block_Bit;
+
+    int i, group_position;
+    group_position = group_id * Cache_L1_Way_Size;
+    
+    
+    for(i = group_position; i < (group_position + Cache_L1_Way_Size); i++){
+        if(cache1[i].valid == 1 && cache1[i].tag == tag_id){ //HIT Cache_1
+
+#ifndef TEST
+            test_time += 2;
+ #endif                  
+
+            return i;
+        }
+    }
+
+    //Fail to hit cache1,replace with random algorithm
+
+    #ifndef TEST
+    test_time += 200;
+#endif
+
+    srand((unsigned int)time(NULL));
+    i = (rand() % Cache_L1_Way_Size) + group_position;
+
+    /*read from memory*/
+    int k;
+    for(k = 0; k < (Cache_L1_Block_Size / BURST_LEN); k++){
+        public_ddr3_read(block_start + (k * BURST_LEN), cache1[i].data+ (k * BURST_LEN));
+    }
+
+    cache1[i].valid = 1;
+    cache1[i].tag = tag_id;
+
+    return i;
+}
+
+
