af437acca9144b4285ba2959ad8aa3cb24cf3240
3019244195-Liu Yan <1967927796@qq.com>
2021-01-17 14:17:37 +0800

    t

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -120,52 +120,54 @@ uint32_t lnaddr_read(lnaddr_t addr, size_t len) {
     // #endif
     uint32_t now_offset = addr & 0xfff;
     if (now_offset + len -1 > 0xfff){
-		// Assert(0,"Cross the page boundary");
-		size_t l = 0xfff - now_offset + 1;
-		uint32_t addr_r = lnaddr_read(addr,l);
-		uint32_t addr_l = lnaddr_read(addr + l,len - l);
-		uint32_t val = (addr_l << (l << 3)) | addr_r;
-		return val;
-	}else {
-		hwaddr_t hwaddr = page_translate(addr);
-		return hwaddr_read(hwaddr,len);
-	}
-	// return hwaddr_read(addr, len);
+	// Assert(0,"Cross the page boundary");
+	size_t l = 0xfff - now_offset + 1;
+	uint32_t addr_r = lnaddr_read(addr,l);
+	uint32_t addr_l = lnaddr_read(addr + l,len - l);
+	uint32_t val = (addr_l << (l << 3)) | addr_r;
+	return val;
+    }
+    else {
+	hwaddr_t hwaddr = page_translate(addr);
+	return hwaddr_read(hwaddr,len);
+    }
+    // return hwaddr_read(addr, len);
 }
 
 void lnaddr_write(lnaddr_t addr, size_t len, uint32_t data) {
 
-/*To Avoid Potential Errors (len = 1 + 3)*/
+    /*To Avoid Potential Errors (len = 1 + 3)*/
 
-// #ifdef DEBUG
-// 	assert(len == 1 || len == 2 || len == 4);
-// #endif
-	uint32_t now_offset = addr & 0xfff;
-	if (now_offset + len - 1 > 0xfff){
-		// Assert(0,"Cross the page boundary");
-		size_t l = 0xfff - now_offset + 1;
-		lnaddr_write(addr,l,data & ((1 << (l << 3)) - 1));
-		lnaddr_write(addr + l,len - l,data >> (l << 3));
-	}else {
+    // #ifdef DEBUG
+    // 	assert(len == 1 || len == 2 || len == 4);
+    // #endif
+    uint32_t now_offset = addr & 0xfff;
+    if (now_offset + len - 1 > 0xfff){
+	// Assert(0,"Cross the page boundary");
+	size_t l = 0xfff - now_offset + 1;
+	lnaddr_write(addr,l,data & ((1 << (l << 3)) - 1));
+	lnaddr_write(addr + l,len - l,data >> (l << 3));
+    }
+    else {
 		hwaddr_t hwaddr = page_translate(addr);
 		hwaddr_write(hwaddr, len, data);
-	}
-	// hwaddr_write(addr, len, data);
+    }
+    // hwaddr_write(addr, len, data);
 }
 
 uint32_t swaddr_read(swaddr_t addr, size_t len) {
 #ifdef DEBUG
-	assert(len == 1 || len == 2 || len == 4);
+    assert(len == 1 || len == 2 || len == 4);
 #endif
-	lnaddr_t lnaddr = seg_translate(addr,len,current_sreg);
-	return lnaddr_read(lnaddr, len);
+    lnaddr_t lnaddr = seg_translate(addr,len,current_sreg);
+    return lnaddr_read(lnaddr, len);
 }
 
 void swaddr_write(swaddr_t addr, size_t len, uint32_t data) {
 #ifdef DEBUG
-	assert(len == 1 || len == 2 || len == 4);
+    assert(len == 1 || len == 2 || len == 4);
 #endif
-	lnaddr_t lnaddr = seg_translate(addr,len,current_sreg);
-	lnaddr_write(lnaddr, len, data);
+    lnaddr_t lnaddr = seg_translate(addr,len,current_sreg);
+    lnaddr_write(lnaddr, len, data);
 }
 
