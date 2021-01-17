a38ec10c4c3c83fcc05d44ecb1f8fd6581025e7d
3019244195-Liu Yan <1967927796@qq.com>
2021-01-17 10:29:32 +0800

    cache.c read_cache2

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -30,7 +30,7 @@ int read_cache1(hwaddr_t address){
 
     uint32_t group_id = (address >> Cache_L1_Block_Bit) & (Cache_L1_Group_Size - 1);
     uint32_t tag_id = (address >> (Cache_L1_Block_Bit + Cache_L1_Group_Bit));
-    uint32_t block_start = (address >> Cache_L1_Block_Bit) << Cache_L1_Block_Bit;
+    //uint32_t block_start = (address >> Cache_L1_Block_Bit) << Cache_L1_Block_Bit;
 
     int i, group_position;
     group_position = group_id * Cache_L1_Way_Size;
@@ -75,6 +75,60 @@ int read_cache1(hwaddr_t address){
 
     cache1[i].valid = 1;
     cache1[i].tag = tag_id;
+
+    return i;
+}
+
+int read_cache2(hwaddr_t address){
+    uint32_t group_id = (address >> Cache_L2_Block_Bit) & (Cache_L2_Group_Size-1);
+    uint32_t tag = address >> (Cache_L2_Block_Bit + Cache_L2_Group_Bit);
+    //set start position of copying address
+    uint32_t block_start = (address >> Cache_L2_Block_Bit) << Cache_L2_Block_Bit;
+
+    int i,group_position;
+    group_position = group_id * Cache_L2_Way_Size;
+
+    for(i = group_position + 0; i < group_position + Cache_L2_Way_Size; i++){
+        if(cache2[i].valid == 1 && cache2[i].tag==tag){
+            //HIT Cache2
+#ifndef TEST
+    test_time += 10;
+#endif
+
+            return i;
+        }
+    }
+
+    //Fail to hit cache2,replace with random algorithm
+
+#ifndef TEST
+    test_time += 200;
+#endif
+
+    srand((unsigned int)time(NULL));
+    i = (rand() % Cache_L2_Way_Size) + group_position;
+
+    /*replace by reading memory*/
+    /*write back*/
+    if(cache2[i].valid == 1 && cache2[i].dirty == 1){
+        uint8_t ret[BURST_LEN << 1];
+        uint32_t block_st = (cache2[i].tag << (Cache_L2_Group_Bit + Cache_L2_Block_Bit)) | (group_id << Cache_L2_Block_Bit);
+        int w;
+        memset(ret,1,sizeof ret);
+        for (w = 0; w < Cache_L2_Block_Size / BURST_LEN; w++){
+            public_ddr3_write(block_st + BURST_LEN * w, cache2[i].data + BURST_LEN * w,ret);
+        }
+    }
+
+    /*read from memory*/
+    int k;
+    for(k = 0; k < (Cache_L2_Block_Size / BURST_LEN); k++){
+        public_ddr3_read(block_start + (k * BURST_LEN), cache2[i].data+ (k * BURST_LEN));
+    }
+
+    cache2[i].dirty = 0;
+    cache2[i].valid = 1;
+    cache2[i].tag = tag;
 
     return i;
 }
